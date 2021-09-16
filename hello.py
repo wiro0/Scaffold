0c27a77e8f789b6d1a592c16fa42efd6b047458f
wiro0 <mehlzwerg@gmail.com>
2021-09-16 21:50:48 +0200

    Extended hello.py test_hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
+'''
 def add(x, y):
     return x + y
-    
+
 result = add(1, 2)
 print("The sum of 1 and 2 is {}".format(result))
+'''
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
